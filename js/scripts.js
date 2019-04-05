061d504e2a6b7c74b679e953674bf2772e5bc343
Katlin <katlin.nbanderson@gmail.com>
2019-04-05 10:18:58 -0700

    add functionality to function that replaces numbers in array based on if they are divisible by 3, or include a 1, 2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,30 @@ function createNumberedArray (number) {
   for (let i =0; i < number; i++) {
     numberedArray.push(i.toString());
   }
-  console.log(numberedArray);
   return numberedArray;
 }
 
-createNumberedArray(25);
-
-function replaceNumbers (array) {
+console.log('95' % 3)
+function replaceNumbers (array, name) {
   var newArray = [];
-
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] % 3 === 0 && array[i] !== '0') {
+      newArray.push("I'm sorry " + name + ". I'm afraid I can't do that")
+    } else if (array[i].includes('3')) {
+      newArray.push("I'm sorry Dave. I'm afraid I can't do that.")
+    } else if (array[i].includes('2')) {
+      newArray.push('Boop!');
+    } else if (array[i].includes('1')) {
+      newArray.push('Beep!');
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  console.log(newArray)
   return newArray;
 }
 
-function replaceNames (array) {
-  var newArray = [];
-
-  return newArray;
-}
+replaceNumbers(createNumberedArray(25), 'Katlin');
 
 //User Interface
 $(function() {
