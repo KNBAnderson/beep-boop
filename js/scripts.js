2659ce042529ed898db74b99f75332abf6e26ed7
Katlin <katlin.nbanderson@gmail.com>
2019-04-05 09:52:16 -0700

    add basic structure to html and scripts

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Business Logic
+function createNumberedArray (number) {
+  var numberedArray;
+
+  return numberedArray;
+}
+
+function replaceNumbers (array) {
+  var newArray = [];
+
+  return newArray;
+}
+
+//User Interface
+$(function() {
+  $('#number').on('input', function() {
+    var number = parseInt($('#number').val());
+  })
+});
