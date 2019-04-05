5d250682464d2510fab760dccf7db6271839beae
Katlin <katlin.nbanderson@gmail.com>
2019-04-05 09:54:07 -0700

    add function that creates a counting array based on an inputed number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 //Business Logic
 function createNumberedArray (number) {
-  var numberedArray;
-
+  var numberedArray = [];
+  for (let i =0; i < number; i++) {
+    numberedArray.push(i.toString());
+  }
+  console.log(numberedArray);
   return numberedArray;
 }
 
+createNumberedArray(25);
+
 function replaceNumbers (array) {
+  var newArray = [];
+
+  return newArray;
+}
+
+function replaceNames (array) {
   var newArray = [];
 
   return newArray;
